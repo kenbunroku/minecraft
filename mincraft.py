505407c88ef3c28f388b4f77ace6fdd17d6a0fa7
kenbunroku <kenbunroku@gmail.com>
2022-02-12 22:32:39 -0600

    Add hand object. Add sound effect.

--- mincraft.py
+++ mincraft.py
@@ -1,3 +1,4 @@
+from turtle import position
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
@@ -7,12 +8,19 @@ stone_texture = load_texture('assets/stone_block.png')
 brick_texture = load_texture('assets/brick_block.png')
 dirt_texture = load_texture('assets/dirt_block.png')
 sky_texture = load_texture('assets/skybox.png')
-
+arm_texture = load_texture('assets/arm_texture.png')
+punch_sound = Audio('assets/punch_sound', loop=False, autoplay=False)
 block_pick = 1
 
 
 def update():
     global block_pick
+
+    if held_keys['left mouse'] or held_keys['right mouse']:
+        hand.active()
+    else:
+        hand.passive()
+
     if held_keys['1']:
         block_pick = 1
     if held_keys['2']:
@@ -38,6 +46,7 @@ class Voxel(Button):
     def input(self, key):
         if self.hovered:
             if key == 'left mouse down':
+                punch_sound.play()
                 if block_pick == 1:
                     voxel = Voxel(position=self.position +
                                   mouse.normal, texture=grass_texture)
@@ -52,6 +61,7 @@ class Voxel(Button):
                                   mouse.normal, texture=dirt_texture)
 
             if key == 'right mouse down':
+                punch_sound.play()
                 destroy(self)
 
 
@@ -66,11 +76,29 @@ class Sky(Entity):
         )
 
 
+class Hand(Entity):
+    def __init__(self):
+        super().__init__(
+            parent=camera.ui,
+            model='assets/arm',
+            texture=arm_texture,
+            scale=0.2,
+            rotation=Vec3(150, -10, 0),
+            position=Vec2(0.4, -0.6))
+
+    def active(self):
+        self.position = Vec2(0.3, -0.5)
+
+    def passive(self):
+        self.position = Vec2(0.4, -0.6)
+
+
 for z in range(20):
     for x in range(20):
         voxel = Voxel(position=(x, 0, z))
 
 player = FirstPersonController()
 sky = Sky()
+hand = Hand()
 
 app.run()
